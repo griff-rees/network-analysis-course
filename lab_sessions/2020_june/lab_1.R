0c7b596e0f0b76e9391d5022a68ff4e5acfdf50d
griff-rees <60181741+griff-rees@users.noreply.github.com>
2020-06-19 10:09:05 +0100

    Update lab_1.R
    Added in group 1's visualisation.

--- lab_sessions/2020_june/lab_1.R
+++ lab_sessions/2020_june/lab_1.R
@@ -2,7 +2,18 @@ library(igraph)
 
 
 # Group 1
-
+```{r}
+library(dplyr)
+library(igraph)
+edges <- read.csv('../data/congress-twitter-network-edges.csv')
+nodes <- read.csv('../data/congress-twitter-network-nodes.csv')
+popular <- nodes[nodes['followers_count'] > 100000,]
+edges <- edges[which( edges$source %in% popular$id_str) ,]
+edges <- edges[which( edges$target %in% popular$id_str) ,]
+g <- graph_from_data_frame(d=edges, vertices=popular, directed=TRUE)
+V(g)$color <- popular$party
+plot(g)
+```
 
 # Group 2
 
